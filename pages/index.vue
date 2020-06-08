43b0046b078d7f22939329bc1da45c75f715e28a
BrentVanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-06-08 08:35:46 +0200

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="w-screen h-screen p-6 overflow-hidden stats-global bg-background text-textColor">
     <p class="text-6xl text-center">Global stats</p>
 
-    <div class="mt-12 align-middle global-stats">
+    <div class="inline-block w-full mt-12 align-middle global-stats">
       <div class="relative float-left w-1/3">
         <p class="text-3xl text-center">Confirmed cases</p>
         <p class="text-6xl text-center" v-if="globalStats">{{globalStats.total_cases}}</p>
@@ -16,11 +16,15 @@
         <p class="text-6xl text-center" v-if="globalStats">{{globalStats.total_deaths}}</p>
       </div>
     </div>
-    <div class="mt-12 align-middle global-stats-chart"></div>
+    <div class="mt-12 align-middle global-stats-chart">
+      <p class="text-4xl text-center">Confirmed cases chart</p>
+      <canvas id="confirmedCasesChart" width="800px" class="mt-3 ml-auto mr-auto" height="400"></canvas>
+    </div>
   </div>
 </template>
 
 <script>
+	import Chart from 'chart.js'
 	export default {
 		name: 'Home',
 
@@ -32,6 +36,7 @@
 
 		mounted() {
 			this.getGlobalStats()
+			this.getGlobalTimelineData()
 		},
 
 		methods: {
@@ -47,6 +52,21 @@
 							error
 						)
 					)
+			},
+
+			getGlobalTimelineData() {
+				this.$store.dispatch('global/getGlobalTimelineData').then(response => {
+					debugger
+				})
+			},
+
+			initCharts() {
+				this.initConfirmedCasesChart()
+			},
+
+			initConfirmedCasesChart() {
+				let ctx = document.querySelector('.confirmedCasesChart')
+				console.log(ctx)
 			}
 		}
 	}
--- store/global.js
+++ store/global.js
@@ -4,7 +4,8 @@ const baseURL = 'https://api.thevirustracker.com/free-api?'
 
 export const state = () => ({
   globalStats: null,
-  countryStats: []
+  countryStats: [],
+  globalTimelineData: []
 })
 
 export const mutations = {
@@ -23,6 +24,10 @@ export const getters = {
 
   getDataForCountry: state => isoCode => {
     return state.countryStats.find(x => x.iso === isoCode)
+  },
+
+  getGlobalTimelineData: state => {
+    return state.globalTimelineData
   }
 }
 
@@ -47,5 +52,21 @@ export const actions = {
       commit('SET_COUNTRY_STATS', { iso: isoCode, data: data })
       return { iso: isoCode, data: data }
     })
-  }
+  },
+
+  getGlobalTimelineData: ({ commit, state }) => {
+    if (state.globalTimelineData.length !== 0) return state.globalTimelineData
+
+    let config = {
+      headers: { 'Access-Control-Allow-Origin': '*' }
+    }
+    return Axios.get(
+      `https://thevirustracker.com/timeline/map-data.json`,
+      config
+    ).then(data => {
+      debugger
+    })
+  },
+
+  getCountryTimelineData: ({ commit, state }, iso2Code) => {}
 }
